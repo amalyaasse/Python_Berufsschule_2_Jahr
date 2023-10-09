12eb8da5a18643ba4736c0227460a4ce236b4aee
Amalya <frauasse@gmail.com>
2023-10-09 16:54:08 +0200

    ohne Commit

--- randome_zahlen_addieren.py
+++ randome_zahlen_addieren.py
@@ -4,17 +4,32 @@
 
 import random
 
-print("addieren Sie die folgende zahlen: ")
-x = random.randint(1,100)
-y = random.randint(1,100)
+falsch = 0
+richtig = 0
 
-print(x,"+",y)
-sum = x + y
-z = float(input("dein Antwort: "))
 
-if(z == sum):
-    print("prima, dein Antwort ist richtig")
-else:
-    print("Leider falsch!")
+for i in range(1,10):
+    print("addieren Sie die folgende zahlen: ")
+    x = random.randint(1,100)
+    y = random.randint(1,100)
+
+    print(x,"+",y)
+    sum = x + y
+    z = float(input("dein Antwort: "))
+   
+   
+    if(z == sum):
+        print("prima, dein Antwort ist richtig")
+        richtig +=1
+        print("-------------nächste Aufgabe-----------------")
+    else:
+        print("Leider falsch!")
+        falsch +=1
+        print("-------------nächste Aufgabe-----------------")
+
+print("                         Ende des Herausforderung                    ")
+print("                         Richtige Antworten:                         ", richtig)
+print("                         Falsche Antworten:                          ",falsch)
+
 
 
